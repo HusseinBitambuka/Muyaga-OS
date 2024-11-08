b8103989bb72954fc03afde5dc71476d09fd75fc
HusseinBitambuka <bitambukahoussein@gmai.com>
2024-11-07 20:29:19 -0500

    setting up the project as a maving project

---
+++ backend/MuyagaOS/src/main/java/com/muyaga/App.java
@@ -0,0 +1,13 @@
+package com.muyaga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ backend/MuyagaOS/src/main/java/com/muyaga/Hardware/Memory.java
@@ -0,0 +1,5 @@
+package com.muyaga.Hardware;
+
+public class Memory {
+
+}
